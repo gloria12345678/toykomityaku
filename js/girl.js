80d6a99c5934bb1e4820fc3847eba8827da3a4ca
成雪.Mu <glorialllmu@gmail.com>
2020-01-03 08:44:32 +0900

    ss
    ss

--- js/girl.js
+++ js/girl.js
@@ -1,5 +1,6 @@
 document.write('<script src="../swiper.min.js"></script>');
 var pcGirlAll = document.getElementById('pcGirlAll');
+var fragment = document.createDocumentFragment();
 var apiList1 = document.getElementById('PcGirlPhoto');
 var myList2 = document.getElementById('catchMynews1');
 var myApi ="https://test-cms-alpha.herokuapp.com/sites/5/profiles.json"
@@ -17,13 +18,17 @@ function loadIndexStaff(){
           girlPhoto.setAttribute("class","swiper-slide");
           // girlPhoto.setAttribute("id","swiper2");
           girlPhoto.innerHTML = '<img src="https://test-cms-alpha.herokuapp.com'+json[i].avatars[0]+'"><div  style="width: 229px;height: 300px;"class="watermark"></div>';
+          fragment.appendChild(girlPhoto);
           pcGirlAll.appendChild(girlPhoto);
-          var swiper = new Swiper('#focus3', {
-            loop : true,
-            autoplay:2000,
-            slidesPerView : 4,
-            slidesPerGroup : 1,
-              });
+          var mySwiper = new Swiper ('#focus3', {
+            loop: true, 
+            speed:1500,
+            autoplay:{
+              delay:0,   
+              disableOnInteraction:false,
+            },
+            slidesPerView:4,
+          });
         }
           for(var i = 0; i < json.length; i++) {
             var girlPhoto =document.createElement("div");
@@ -67,7 +72,59 @@ function loadnews(){
   loadIndexStaff()
   loadnews()
 
-
-
-
- 
+  var  schedulesGirls= document.getElementById('schedule'); 
+  var myApi3 ="https://test-cms-alpha.herokuapp.com/sites/1/schedules.json"
+  function addNewSchedules(MyJson){
+   for(let i = 0; i < MyJson.length; i++){
+     let ss =new Date(MyJson[i].period)
+     let t =ss.toString();
+     let addDate = t.substring(4,16);
+     let addDate1 = t.substring(4,11);
+     let addtime1 = t.substring(16,18);
+     let addtime2 = t.substring(19,21);
+     MyJson[i].date= addDate;
+     MyJson[i].date1= addDate1;
+     MyJson[i].profile.time= addtime1+'時〜'+addtime2+'時';
+   }
+   function sortNumber(x, y){
+     return  new Date(x.date)-new Date(y.date).getTime();
+   }
+   return MyJson.sort(sortNumber)
+  }
+  function groupBy( array , f ) {
+   let groups = {};
+   array.forEach( function( o ) {
+     let group = JSON.stringify( f(o) );
+       groups[group] = groups[group] || [];
+       groups[group].push( o );
+       
+   });
+   return Object.keys(groups).map( function(group) {
+       return groups[group];
+   });
+  }
+  function loadMySchedules(){
+   let xhr = new XMLHttpRequest();
+   xhr.open("GET",myApi3,true);
+   xhr.onload = function(){
+     if(this.status == 200){
+       let json =new Array
+       json = JSON.parse(this.responseText);
+       console.log(json);
+        let newList = addNewSchedules(json)
+       let sorted = groupBy(newList, function(item){
+         return [item.date];
+       })
+      console.log(sorted);
+         for(let j = 0; j < sorted[0].length; j++){
+           var photo =document.createElement("div");
+           photo.setAttribute("class","photo");
+           photo.innerHTML = '<div class="l"><div class="watermark"></div><img src="https://test-cms-alpha.herokuapp.com'+sorted[0][j].profile.avatars[0]+'"></div><div>'+sorted[0][j].profile.name+''+sorted[0][j].profile.age+'</div><div>'+sorted[0][j].profile.time+'</div>';
+           schedulesGirls.appendChild(photo);
+        
+           };
+     }          
+   }
+   xhr.send();
+   }
+   loadMySchedules()
